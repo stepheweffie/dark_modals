3c88c2272981f2de1a265dcfab926264fd907256
stepheweffie <stephie.maths@gmail.com>
2023-12-14 07:12:33 -0500

    notifications

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from nicegui import ui
 
 
 @ui.page(path='/')
-def dark_modals():
+async def dark_modals():
 
     modal = ui.element('modal').classes('rounded-lg shadow-lg').style(
                 'min-width: 70%; z-index: 999; position: fixed;'
@@ -29,14 +29,12 @@ def dark_modals():
 
     def set_modal_bg(color: str) -> None:
         ui.query('modal').style(replace=f'background-color: {color}')
-        modal_colors['bg_color'] = color
         if color is dark_bg_color:
             ui.query('modal').style(replace=f'color: {dark_text_color}')
         else:
             ui.query('modal').style(replace=f'color: {_text_color}')
 
     def toggle_dark_mode(switch_state, _modal):
-        _bg_color = modal_colors['bg_color']
         bg_color = f'{dark_bg_color}' if switch_state is True else f'{_bg_color}'
         text_color = f'{dark_text_color}' if switch_state is True else f'{_text_color}'
         # Additional styles for the modal template
@@ -45,7 +43,17 @@ def dark_modals():
             f'z-index: 666; background-color: {bg_color}; color: {text_color};')
         return _modal
 
-    async def user_modal() -> None:
+    async def try_submit(name, email, bg) -> None:
+        if name.value and email.value:
+            name.value = ''
+            email.value = ''
+            # await email validation
+            bg_modal.remove(bg)
+            ui.notify(f'Please Check {email.value}', color='positive')
+        else:
+            ui.notify('Please fill out all fields', color='negative')
+
+    def get_modal():
         with bg_modal:
             # Without a new modal the modal will not close
             bg = toggle_dark_mode(dark_mode_switch.value, bg_modal)
@@ -59,15 +67,19 @@ def dark_modals():
                 ui.label('Custom Modal').classes('text-2xl')
                 with ui.row().classes('w-full items-center px-4'):
                     ui.label('Name').classes('text-xl')
-                    ui.input(label='Name', placeholder='Enter Name')
+                    name = ui.input(label='Name', placeholder='Enter Name')
                 with ui.row().classes('w-full items-center px-4'):
                     ui.label('Email').classes('text-xl')
-                    ui.input(label='Email', placeholder='Enter Email')
+                    email = ui.input(label='Email', placeholder='Enter Email').on('keydown.enter', try_submit)
                 with ui.row().classes('w-full items-center px-4'):
-                    ui.button('Submit', on_click=lambda: bg_modal.remove(bg)).classes('text-xl w-72')
+                    submit = ui.button('Submit').classes('text-xl w-72').on('click',
+                                                                            lambda: try_submit(name, email, bg))
 
     # Open the modals
-    ui.button('Open Modal').on('click', user_modal)
+    ui.button('Open Modal').on('click', get_modal)
+    # await app.storage.user.get('authenticated', False)
+    # if app.storage.user.get('authenticated', False):
+    #     return RedirectResponse('/')
 
 
 ui.run()
